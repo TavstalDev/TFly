91ab125c001ca555be847a17a4391ef81bbbc9b7
TavstalDev <tavstal@gmail.com>
2024-07-08 18:10:53 +0200

    Minor changes

--- TFly/Properties/AssemblyInfo.cs
+++ TFly/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TFly")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Basic flight rocketMod plugin for Unturned.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tavstal")]
 [assembly: AssemblyProduct("TFly")]
 [assembly: AssemblyCopyright("Copyright © 2024 Tavstal")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
